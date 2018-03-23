fe9aa41555abf076299d0ce62c433aaffc0a372a
141250100 <141250100@smail.nju.edu.cn>
2018-03-23 11:42:38 +0800

    mainFrame

---
+++ src/main/java/com/example/demo/OldneighborhoodSalerInfoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldneighborhoodSalerInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldneighborhoodSalerInfoApplication.class, args);
+	}
+}
